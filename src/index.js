58e05b363acc1085c05bd78301224d1f4e403fec
Ayush Rawal <ayushrawal12@gmail.com>
2018-05-17 21:45:08 +0000

    automatically backed up by learn

--- src/index.js
+++ src/index.js
@@ -1,23 +1,26 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-let meInReact = (props) => (
-  <div className="me">
-  <h1>An Awesome Person</h1>
-<p>Who is learning React</p>
+class meInReact extends React.Component {
+  render () {
+    return (
+      <div className="me">
+      <h1>An Awesome Person</h1>
+    <p>Who is learning React</p>
 
-<ul class="my-interests">
-<li>JavaScript</li>
-<li>React</li>
-<li>Movies</li>
-<li>Ice cream</li>
-</ul>
-  </div>
-)
-
+    <ul class="my-interests">
+    <li>JavaScript</li>
+    <li>React</li>
+    <li>Movies</li>
+    <li>Ice cream</li>
+    </ul>
+      </div>
+    )
+  }
+}
 // IMPORTANT!!!! uncomment the lines below to make your tests work
 ReactDOM.render(
-  meInReact(),
+  meInReact,
   document.getElementById('global')
 );
 
