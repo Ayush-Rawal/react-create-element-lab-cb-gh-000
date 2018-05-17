7ed83dea868058ddf551df0ba14920d3a79b6464
Ayush Rawal <ayushrawal12@gmail.com>
2018-05-17 21:55:05 +0000

    automatically backed up by learn

--- src/index.js
+++ src/index.js
@@ -1,27 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-class MeInReact extends React.Component {
-  render () {
-    return (
-      <div className="me">
-      <h1>An Awesome Person</h1>
-    <p>Who is learning React</p>
+const list1 = React.createElement('li', {}, 'JavaScript') 
+const list2 = React.createElement('li', {}, 'React')
+const list3 = React.createElement('li', {}, 'Movies') 
+const list4 = React.createElement('li', {}, 'Ice cream') 
 
-    <ul className="my-interests">
-    <li>JavaScript</li>
-    <li>React</li>
-    <li>Movies</li>
-    <li>Ice cream</li>
-    </ul>
-      </div>
-    )
-  }
-}
+const list = React.createElement('ul', {className: 'my-interests'}, [list1, list2, list3, list4])
+const heading = React.createElement('h1', {}, 'An Awesome Person')
+const para = React.createElement('p', {}, 'Who is learning React')
+
+const meInReact = React.createElement('div', {className: 'me'}, [heading, para, list])
+
 // IMPORTANT!!!! uncomment the lines below to make your tests work
 ReactDOM.render(
-  <MeInReact/>,
+  meInReact,
   document.getElementById('global')
 );
 
-export default MeInReact
+export default meInReact
